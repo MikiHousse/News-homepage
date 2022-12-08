ae575363bf202a70da6bfcbbd219924a25949bd9
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2022-12-08 17:22:27 +0300

    Start

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        castomWhite: 'hsl(36, 100%, 99%)',
+        castomGray: 'hsl(233, 8%, 79%)',
+        castomDark: 'hsl(236, 13%, 42%)',
+        castomVeryDark: 'hsl(240, 100%, 5%)'
+      }
+    },
+  },
+  plugins: [],
+}
